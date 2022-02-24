6463ea33c4dd440276d9907a7a90a2b31280c2ec
OndrejNepozitek <ondra@nepozitek.cz>
2022-02-24 10:33:13 +0100

    Exclude example pages from generate pdf

--- src/utils.ts
+++ src/utils.ts
@@ -77,7 +77,7 @@ export async function generatePDF({
       );
 
       // Make joined content html
-      if (excludeURLs && excludeURLs.includes(nextPageURL) && nextPageURL.includes("/examples/")) {
+      if ((excludeURLs && excludeURLs.includes(nextPageURL)) || nextPageURL.includes("/examples/")) {
         console.log(chalk.green('This URL is excluded.'));
       } else {
         contentHTML += html;
